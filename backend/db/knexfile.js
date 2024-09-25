b69e81b2a8d39a6d15795d54fee00e2a35108149
ZackO <zo@pyra.dk>
2024-09-25 13:02:50 +0200

    setup knex, docker, frontend and eslint.

---
+++ backend/db/knexfile.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import pkg from 'knex';
+const { Knex } = pkg;
+
+
+
+// Load environment variables from a .env file
+dotenv.config();
+
+const config = {
+    client: 'mysql2',
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    },
+    migrations: {
+        directory: '.migrations',
+    },
+    seeds: {
+        directory: '.seeds',
+    },
+};
+
+export default config;
