1a0fa5f46e4969e1dd8ab96fe5c9dbd57bc1ec05
ZackO <zo@pyra.dk>
2024-10-15 17:28:38 +0200

    attempt 40

--- backend/db/db.js
+++ backend/db/db.js
@@ -7,10 +7,10 @@ if (!process.env.GITHUB_ACTIONS) {
 const db = knex({
     client: 'mysql2',
     connection: {
-        host:  "mysql",
-        user: "test",
-        password: "test",
-        database: "test",
+        host: process.env.DB_HOST || "localhost",
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     },
 });
 
