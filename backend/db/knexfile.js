8dcb5e63353d5fcec01c653e86ff8128554559cf
ZackO <zo@pyra.dk>
2024-10-15 12:09:12 +0200

    fix knex

--- backend/db/knexfile.js
+++ backend/db/knexfile.js
@@ -8,7 +8,7 @@ dotenv.config({path: "../../.env"});
 const config = {
   client: "mysql2",
   connection: {
-    host: process.env.DB_HOST,
+    host: "localhost",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
